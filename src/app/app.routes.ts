23a67a92e1da51661fd7a0144c763141f5ff58ee
Starcrash16 <renerosendo8@gmail.com>
2025-06-14 16:40:13 -0600

    correciones de versiones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,25 +2,11 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from '../components/inicio/inicio.component';
 
 export const routes: Routes = [
-<<<<<<< HEAD
   {
     path:'sesion',
     loadChildren: () => import('./features/auth/auth.routes'), // Usa m.routes (no m.authRoutes)
   },
-
-];
-=======
-<<<<<<< HEAD
-    
     {path:'inicio',component:InicioComponent},
     {path:'**',pathMatch:'full',redirectTo:'inicio'}
 ];
-=======
 
-  {
-    path:'sesion',
-    loadChildren: () => import('./features/auth/auth.routes'), // Usa m.routes (no m.authRoutes)
-  }
-];
->>>>>>> bf1995c997c94836742235304106ed5e9450bcab
->>>>>>> c8ddcf0bb8aa9c08062f760c9ab56b76b0a8b76c
