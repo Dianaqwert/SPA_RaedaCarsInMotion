8813479991df86ba68907f7c91766c4432fa164a
Asus Ryzen 5 <esmeralda298rivera@gmail.com>
2025-06-14 16:23:49 -0600

    Commit: imagenes  y componentes esemciales integrados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../components/navbar/navbar/navbar.component';
+import { FooterComponent } from '../components/footer/footer/footer.component';
+import { InicioComponent } from "../components/inicio/inicio.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, NavbarComponent, FooterComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'SPA_RaedaCarsInMotion';
 }
