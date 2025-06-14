e9da210c16546af9e8eca7531373e78b9f5fd3fe
Starcrash16 <renerosendo8@gmail.com>
2025-06-14 17:05:35 -0600

    Corrección de versiones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,14 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../components/navbar/navbar/navbar.component';
 import { FooterComponent } from '../components/footer/footer/footer.component';
 import { InicioComponent } from "../components/inicio/inicio.component";
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, NavbarComponent, FooterComponent, InicioComponent],
-=======
-import { RouterOutlet } from '@angular/router';
 import { NgxSonnerToaster } from 'ngx-sonner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgxSonnerToaster],
->>>>>>> bf1995c997c94836742235304106ed5e9450bcab
+  imports: [RouterOutlet, RouterModule, NavbarComponent, FooterComponent, InicioComponent, NgxSonnerToaster],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
