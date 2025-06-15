426d4ac28dccc0a4283b99c56fd5decd6bff16bf
Starcrash16 <renerosendo8@gmail.com>
2025-06-15 17:04:25 -0600

    Sign In y Sign Up v3, funcionales. Nav Bar funcional con nombre de usuario. Rutas de panel de admin y solicitudes: correcto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../components/navbar/navbar/navbar.component';
 import { FooterComponent } from '../components/footer/footer/footer.component';
-import { InicioComponent } from "../components/inicio/inicio.component";
 import { NgxSonnerToaster } from 'ngx-sonner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, NavbarComponent, FooterComponent, InicioComponent, NgxSonnerToaster],
+  imports: [RouterOutlet, RouterModule, NavbarComponent, FooterComponent, NgxSonnerToaster],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,28 @@ import { InicioComponent } from '../components/inicio/inicio.component';
 
 export const routes: Routes = [
   {
-    path:'sesion',
-    loadChildren: () => import('./features/auth/auth.routes'), // Usa m.routes (no m.authRoutes)
+    path: 'sesion',
+    loadChildren: () => import('./features/auth/auth.routes'),
   },
-    {path:'inicio',component:InicioComponent},
-    {path:'**',pathMatch:'full',redirectTo:'inicio'}
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path:'sesion',
+    loadChildren: () => import('./features/auth/auth.routes'),
+  },
+  {
+    path: 'admin/panel',
+    loadComponent: () => import('./features/panel/admin/admin.component'),
+  },
+  {
+    path: 'solicitudes',
+    loadComponent: () => import('./features/panel/solicitudes/solicitudes.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'inicio'
+  }
 ];
 
