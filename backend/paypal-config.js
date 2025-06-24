c3a597f8c32cb6c5c09d72834c8f2e7ca81d8c4e
Ernesto Morquecho <alonsomrqch@gmail.com>
2025-06-23 21:32:09 -0600

    Api de paypal agregada, hacer pruebas

---
+++ backend/paypal-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  clientId: 'AY_hfMBiPZ76XKu4GzRGnn7lE3Xx74uHqM2wQYMHlinVUdkjBqwjQ4P8u4EfjQqclTq0L-UnxxbGuMcz', // Your sandbox client ID
+  clientSecret: 'EB6XMTjMIuhsPKPLj85jj9Be7Lif7_2Pfy4tFxVVAnjMPsO3iYhSihohwrA_dJxqsiJWSI1QpG2OtPPK', // Your sandbox secret
+  baseUrl: 'https://api-m.sandbox.paypal.com' // PayPal sandbox API URL
+};
--- src/configPayPal.ts
+++ src/configPayPal.ts
@@ -1,4 +1,4 @@
 export const payPalConfig = {
   idPayPal: 'AY_hfMBiPZ76XKu4GzRGnn7lE3Xx74uHqM2wQYMHlinVUdkjBqwjQ4P8u4EfjQqclTq0L-UnxxbGuMcz', // ID del cliente de sandbox
-  baseUrl: 'https://sandbox.paypal.com' // URL del servidor de sandbox
+  baseUrl: 'https://api-m.sandbox.paypal.com' // URL del servidor de sandbox
 };
