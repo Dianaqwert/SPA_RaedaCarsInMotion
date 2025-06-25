142e2d1c38d0536e8917b12378e0d31b049a9ed9
OmarEOAD <erikalbadavila@gmail.com>
2025-06-24 22:43:13 -0600

    pipe en equipo

--- src/components/equipo/equipo/equipo.component.ts
+++ src/components/equipo/equipo/equipo.component.ts
@@ -8,11 +8,12 @@ import {MatChipsModule} from '@angular/material/chips';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatIconModule} from '@angular/material/icon';
+import { CustomUppercasePipe } from '../../../app/shared/pipes/custom-uppercase.pipe';
 
 @Component({
   selector: 'app-equipo',
-  imports: [MatCardModule,MatButtonModule,MatChipsModule,MatProgressBarModule,MatSlideToggleModule,MatGridListModule,MatIconModule
-  ],
+  imports: [MatCardModule,MatButtonModule,MatChipsModule,MatProgressBarModule,MatSlideToggleModule,MatGridListModule,CustomUppercasePipe,
+            MatIconModule],
   templateUrl: './equipo.component.html',
   styleUrl: './equipo.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
